e881cbeccf581a94dfefd32aa60d0e3474114d9b
sinore69 <saptarshidutta599@gmail.com>
2025-07-06 13:59:38 +0530

    nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,7 +14,9 @@ return {
         "--hidden",
         "--no-ignore",
         "--glob=!.git/",
+        "--glob=!node_modules/**",
       },
+      file_ignore_patterns = { "node_modules" },
     })
 
     opts.pickers = vim.tbl_deep_extend("force", opts.pickers or {}, {
